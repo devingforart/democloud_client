50f580a21c65aff9f06416d2fbc3b146e2a2e95d
unknown <devingforart@gmail.com>
2024-10-04 20:05:26 -0300

    port

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import preact from '@preact/preset-vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  server: {
+    host: '127.0.0.1',  // Forzar IPv4 en lugar de IPv6 (::1)
+    port: 5173          // Asegúrate de que es el puerto correcto
+  }
 })
